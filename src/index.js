5c0e568081b826c62137c2aa3db6e03a94135339
belleomoraes <iolliveira.moraes@gmail.com>
2022-08-26 16:26:16 -0300

    feat show last 10 tweets

--- src/index.js
+++ src/index.js
@@ -7,19 +7,36 @@ app.use(express.json());
 
 let user = [];
 const userServer = [];
-const tweetServer = []
+const tweetServer = [];
 let tweet = [];
 
 app.post("/sign-up", (req, res) => {
   user = req.body;
   userServer.push(user);
-  res.send("OK");
+  res.status(200).send("OK");
 });
 
 app.post("/tweets", (req, res) => {
-tweet = req.body;
-tweetServer.push(tweet)
-console.log(tweetServer)
-res.send("OK");
-})
-app.listen(5000, () => console.log("Listennig on 5000"));
+  tweet = req.body;
+  tweetServer.push(tweet);
+  console.log(tweetServer);
+  res.status(200).send("OK");
+});
+
+app.get("/tweets", (req, res) => {
+  let lastTweets = [];
+  if (tweetServer.length <= 10) {
+    lastTweets = tweetServer;
+  } else {
+    let positionSlice = teste.length - 10;
+    lastTweets = tweetServer.slice(positionSlice);
+  }
+  console.log(user);
+  let lastTweetsAvatar = lastTweets.map((tweet) => {
+    tweet.avatar = user.avatar;
+    return tweet;
+  });
+  res.send(lastTweetsAvatar);
+});
+
+app.listen(5000, () => console.log("Listenning on 5000"));
