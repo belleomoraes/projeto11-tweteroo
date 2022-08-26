69f1dc0029540868edecd02d53279644d69631e4
belleomoraes <iolliveira.moraes@gmail.com>
2022-08-26 11:18:41 -0300

    feat saving user and tweet on server via post

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
-import express from 'express'
-import cors from 'cors'
+import express from "express";
+import cors from "cors";
 
-const app = express()
-app.use(cors())
-app.use(express.json())
+const app = express();
+app.use(cors());
+app.use(express.json());
 
+let user = [];
+const userServer = [];
+const tweetServer = []
+let tweet = [];
 
-app.listen(5000, () => console.log("Listennig on 5000"))
+app.post("/sign-up", (req, res) => {
+  user = req.body;
+  userServer.push(user);
+  res.send("OK");
+});
+
+app.post("/tweets", (req, res) => {
+tweet = req.body;
+tweetServer.push(tweet)
+console.log(tweetServer)
+res.send("OK");
+})
+app.listen(5000, () => console.log("Listennig on 5000"));
